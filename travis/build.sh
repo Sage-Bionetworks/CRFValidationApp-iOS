c67df37da6052253591ebba8680d042c10284b45
Khai Do <zaro0508@gmail.com>
2017-09-26 14:25:02 -0700

    Move tests to post merge build
    Pre merge testings has been disabled because Travis does not support
    private data in PR builds.  Testing can only be done post merge.

--- travis/build.sh
+++ travis/build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -ex
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
-    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane scan
+    echo "Pre merge testings has been disabled because Travis does not support private data in PR builds"
+    # git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
+    # FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane scan
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
+    # Testing is done post merge
+    git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane scan
     bundle exec fastlane ci_archive scheme:"CRFModuleValidation" export_method:"app-store"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane scan
