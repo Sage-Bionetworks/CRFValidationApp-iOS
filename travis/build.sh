ad4b3d1040786115bef2fd887adb1bd728cdd746
Khai Do <zaro0508@gmail.com>
2017-09-25 21:10:27 -0700

    setup project for travis build

---
+++ travis/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -ex
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane scan
+elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
+    bundle exec fastlane ci_archive scheme:"CRFModuleValidation" export_method:"app-store"
+elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane scan
+    bundle exec fastlane beta scheme:"CRFModuleValidation" export_method:"app-store"
+fi
+exit $?
