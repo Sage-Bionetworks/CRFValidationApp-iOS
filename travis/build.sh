60556cda58cac35be485744901ec133f0ebcca68
Khai Do <zaro0508@gmail.com>
2017-09-29 07:54:53 -0700

    test build timeout

--- travis/build.sh
+++ travis/build.sh
@@ -11,7 +11,7 @@ elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag com
     bundle exec fastlane ci_archive scheme:"CRFModuleValidation" export_method:"app-store"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
     git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
-    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane scan
+#    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane scan
     bundle exec fastlane beta scheme:"CRFModuleValidation" export_method:"app-store"
 fi
 exit $?
