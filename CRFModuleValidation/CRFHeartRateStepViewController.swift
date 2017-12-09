08f0f7e6d44961ded74c4ce2d9167b734639fd22
Shannon Young <shannon.young@sagebase.org>
2017-12-08 17:28:34 -0800

    Update heart rate recorder to save video recording.

--- CRFModuleValidation/CRFHeartRateStepViewController.swift
+++ CRFModuleValidation/CRFHeartRateStepViewController.swift
@@ -37,18 +37,15 @@ import ResearchSuite
 
 public class CRFHeartRateStepViewController: RSDActiveStepViewController, RSDAsyncActionControllerDelegate {
     
-    /**
-     */
+    /// The heart rate recorder.
     public private(set) var bpmRecorder: CRFHeartRateRecorder?
     
-    /**
-     This step has multiple results so use a collection result to store them.
-     */
+    /// This step has multiple results so use a collection result to store them.
     public private(set) var collectionResult: RSDCollectionResult?
     
-    /**
-     Add the result to the collection. This will fail to add the result if called before the step is added to the view controller.
-     */
+    /// Add the result to the collection. This will fail to add the result if called before the step is
+    /// added to the view controller.
+    /// - parameter result: The result to add to the collection.
     public func addResult(_ result: RSDResult) {
         guard step != nil else { return }
         var stepResult = self.collectionResult ?? RSDCollectionResultObject(identifier: self.step.identifier)
@@ -80,7 +77,9 @@ public class CRFHeartRateStepViewController: RSDActiveStepViewController, RSDAsy
         
         // Create a recorder that runs only during this step
         let taskPath = self.taskController.taskPath!
-        let config = CRFHeartRateRecorderConfiguration(identifier: "recorder")
+        var config = CRFHeartRateRecorderConfiguration(identifier: "recorder")
+        config.shouldSaveBuffer = true  // TODO: refactor to allow setting up the config using json file.
+        config.duration = self.activeStep?.duration ?? config.duration
         bpmRecorder = CRFHeartRateRecorder(configuration: config, outputDirectory: taskPath.outputDirectory)
         bpmRecorder?.delegate = self
         
@@ -116,7 +115,13 @@ public class CRFHeartRateStepViewController: RSDActiveStepViewController, RSDAsy
         recorder.stop {[weak self] (_, result, error) in
             DispatchQueue.main.async {
                 if result != nil {
-                    self?.addResult(result!)
+                    if let collectionResult = result as? RSDCollectionResult {
+                        for childResult in collectionResult.inputResults {
+                            self?.addResult(childResult)
+                        }
+                    } else {
+                        self?.addResult(result!)
+                    }
                 }
                 // TODO: syoung 11/08/2017 Add error to result set?
                 self?._goNext()
@@ -153,6 +158,9 @@ public class CRFHeartRateStepViewController: RSDActiveStepViewController, RSDAsy
     private var _isCoveredObserver: NSKeyValueObservation?
     
     private func _startCountdownIfNeeded() {
+        if _markTime == nil {
+            _markTime = ProcessInfo.processInfo.systemUptime
+        }
         guard startUptime == nil else { return }
         self.start()
     }
@@ -165,6 +173,7 @@ public class CRFHeartRateStepViewController: RSDActiveStepViewController, RSDAsy
                 // zero out the BPM to indicate to the user that they need to cover the flash
                 // and show the initial instruction.
                 self.progressLabel?.text = "--"
+                self._markTime = nil
                 if let instruction = self.activeStep?.spokenInstruction(at: 0) {
                     self.instructionLabel?.text = instruction
                 }
@@ -185,12 +194,19 @@ public class CRFHeartRateStepViewController: RSDActiveStepViewController, RSDAsy
         return numberFormatter
     }()
     
+    private var _encouragementGiven: Bool = false
+    private var _markTime: TimeInterval?
+    
     private func _updateBPMLabel(_ bpm: Int) {
         if self.collectionResult?.inputResults.count ?? 0 == 0 {
             // Add the starting heart rate as a result for display to the user
             var bpmResult = RSDAnswerResultObject(identifier: "\(self.step.identifier)_start", answerType: RSDAnswerResultType(baseType: .decimal))
             bpmResult.value = bpmRecorder?.bpm
             addResult(bpmResult)
+        } else if !_encouragementGiven, let markTime = _markTime, (ProcessInfo.processInfo.systemUptime - markTime) > 40,
+            let continueText = self.uiStep?.detail {
+            _encouragementGiven = true
+            self.speakInstruction(continueText, at: 40, completion: nil)
         }
         
         self.progressLabel?.text = numberFormatter.string(from: NSNumber(value: bpm))
