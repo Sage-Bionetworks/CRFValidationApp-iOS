719df297468b848de703ce4b142396ca00a2de4f
Khai Do <zaro0508@gmail.com>
2018-01-18 13:35:17 -0800

    Setup enterprise build
    New certs and profiles were added to ios-certificates repo.
    This change configures fastlane to generate enterprise builds.
    It also stops deployments to testflight.

--- travis/build.sh
+++ travis/build.sh
@@ -8,10 +8,10 @@ elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag com
     # Testing is done post merge
     git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane scan
-    bundle exec fastlane ci_archive scheme:"CRFModuleValidation" export_method:"app-store"
+    bundle exec fastlane ci_archive scheme:"CRFModuleValidation" export_method:"enterprise"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
     git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
 #    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane scan
-    bundle exec fastlane beta scheme:"CRFModuleValidation" export_method:"app-store"
+    bundle exec fastlane beta scheme:"CRFModuleValidation" export_method:"enterprise"
 fi
 exit $?
